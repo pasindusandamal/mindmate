1135a81ce7473a6cfab6d8b69f7007677940b47b
pasindusandamal <pkpsandamal@students.nsbm.ac.lk>
2024-11-21 22:51:41 +0530

    Enhance light theme with updated color scheme and improved AppBar styling

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -3,14 +3,22 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
-      primarySwatch: Colors.deepPurple,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.deepPurple,
+        brightness: Brightness.light,
+      ),
       visualDensity: VisualDensity.adaptivePlatformDensity,
       appBarTheme: AppBarTheme(
-        elevation: 0,
+        elevation: 4,
         backgroundColor: Colors.deepPurple,
         centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
       ),
-      scaffoldBackgroundColor: Colors.white,
+      scaffoldBackgroundColor: Colors.grey[100],
       inputDecorationTheme: InputDecorationTheme(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
